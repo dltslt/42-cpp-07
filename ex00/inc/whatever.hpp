3eb4d426f148b7587cb0518cd1f32c47d93c1a6b
dltslt <87982398+dltslt@users.noreply.github.com>
2026-04-20 02:17:34 +0200

    feat: ex00

---
+++ ex00/inc/whatever.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 01:11:59 by mweghofe          #+#    #+#             */
+/*   Updated: 2026/04/20 02:17:21 by mweghofe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_hPP
+# define WHATEVER_HPP
+
+/*
+	REMEMBER
+	- templates create version for every (used?) datatype on compile time
+	- the datatype must support all operators used by the function template
+	- explicit calling is possible (but here useless): min<int>()
+*/
+
+template <typename T> void swap(T& left, T& right)
+{
+	T tmp = left; // this is a copy operation, like classes call copy ctor
+	left = right;
+	right = tmp;
+}
+
+template <typename T> T min(const T& one, const T& two)
+{
+	if (one < two)
+		return (one);
+	else
+		return (two);
+}
+
+template <typename PLACEHOLDER>
+PLACEHOLDER max(const PLACEHOLDER& one, const PLACEHOLDER& two)
+{
+	if (one > two)
+		return (one);
+	else
+		return (two);
+}
+
+#endif
---
+++ ex00/src/main.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 01:13:54 by mweghofe          #+#    #+#             */
+/*   Updated: 2026/04/20 02:16:19 by mweghofe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "##### SUBJECT TESTS\n\n";
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	std::cout << "\n##### SOME MORE TESTS\n\n";
+	a = -42;
+	b = 42;
+	double d1 = 42.24, d2 = 24.42;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << max(a, b) << std::endl;
+	std::cout << "d1 = " << d1 << ", d2 = " << d2 << std::endl;
+	std::cout << "min( d1, d2 ) = " << min(d1, d2) << std::endl;
+	std::cout << "max( d1, d2 ) = " << max(d1, d2) << std::endl;
+	std::cout << "swapping d1&d2...\n";
+	swap(d1, d2);
+	std::cout << "d1 = " << d1 << ", d2 = " << d2 << std::endl;
+	std::cout << "\n##### GENERAL DANGLING POINTER RISK\n\n";
+	const std::string& ref = std::string("a life-extended temporary object");
+	std::cout << "some temporary objects receive an extended life time. "
+			  << "such as this <" << ref << ">\n";
+	const std::string& alias = ::max(ref, std::string("dangling reference"));
+	std::cout << "others like <alias> hold a dangling reference and are UB\n";
+	const int& dont = ::max(int(24), int(42));
+	std::cout << "this is also true for references to simple types like <int>\n";
+	int okay = ::max(int(24), int(42));
+	std::cout << "this one is safe - it is not a reference, the type is a copy:"
+			  << " <" << okay << ">\n";
+	(void)alias;
+	(void)dont;
+	return (0);
+}
