9ed9bfbe1c96c259a2882d3cdf71c124102e37c0
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2026-04-21 15:20:04 +0200

    refactor: used std::size_t

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 02:18:05 by mweghofe          #+#    #+#             */
-/*   Updated: 2026/04/20 23:14:15 by mweghofe         ###   ########.fr       */
+/*   Updated: 2026/04/21 15:19:08 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,22 @@
 */
 
 template <typename T>
-void iter(T* array, const unsigned long len, void (*f)(const T&))
+void iter(T* array, const std::size_t len, void (*f)(const T&))
 {
 	if (array == NULL || f == NULL)
 		return ;
-	for (unsigned long i = 0; i < len; i++)
+	for (std::size_t i = 0; i < len; i++)
 	{
 		f(array[i]);
 	}
 }
 
 template <typename T>
-void iter(T* array, const unsigned long len, void (*f)(T&))
+void iter(T* array, const std::size_t len, void (*f)(T&))
 {
 	if (array == NULL || f == NULL)
 		return ;
-	for (unsigned long i = 0; i < len; i++)
+	for (std::size_t i = 0; i < len; i++)
 	{
 		f(array[i]);
 	}
